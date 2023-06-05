3b74462c30e285447cee3c2d009b37a062a66759
DamianT <elmo@DESKTOP-OP1RDAI>
2023-06-05 17:02:21 -0600

    err:el servidor no responde las request

--- index.js
+++ index.js
@@ -1,55 +1,75 @@
 const http = require("http");
 
+const routes = {
+    "/hola": {
+        GET: "Estamos en el endpoint de hola",
+        POST: "Estamos creando algo en hola",
+        PUT: "Estamos sustituyendo algo en hola",
+        PATCH: "Estamos parcheando algo en hola",
+        DELETE: "Estamos borrando en hola"
+    },
+    "/adios": {
+        GET: "Estamos en el endpoint de adios",
+        POST: "Estamos posteando en adios",
+        PUT: "Estamos sustituyendo en adios",
+        PATCH: "Estamos parchando algo en adios",
+        DELETE: "Estamos borrando en adios"
+    }
+};
 
-//aqui se declara el servidor}
-const server = http.createServer((request,response)=>{
-
-    // Leer la request 
-
-    // console.log(request.headers)
-
-
-    const {url,method}= request
-    if (method === "GET" && url === "/hola") {
-        response.write("Estamos en el endpoint de hola")
+const newServer = http.createServer((request,response) => {
+    const {url,method}= request;
+    if (routes[url] && routes[url][method]) {
+        response.write(routes[url][method]);
+    }else {
+        response.write("no existe el nodo al que quieres acceder")
     }
-    if (method === "POST" && url === "/hola") {
-        response.write("Estamos creando algo en hola")
-    }
-    if (method === "PUT" && url === "/hola") {
-        response.write("Estamos sustituyendo algo en hola")
-    }
-    if (method === "PATCH" && url === "/hola") {
-        response.write("Estamos parcheando algo en hola")
-    }
-    if (method === "DELETE" && url === "/hola") {
-        response.write("Estamos borrando en hola")
-    }
-    
-    if (method === "GET" && url === "/adios") {
-        response.write("Estamos en el endpoint de adios")
-    }
-    if (method === "POST" && url === "/adios") {
-        response.write("Estamos posteando en adios")
-    }
-    if (method === "PUT" && url === "/adios") {
-        response.write("Estamos sustituyendo en adios")
-    }
-    if (method === "PATCH" && url === "/adios") {
-        response.write("Estamos parchando algo en adios")
-    }
-    if (method === "DELETE" && url === "/adios") {
-        response.write("Estamos borrando en adios")
-    }
-    
-    // responder las request
-    // response.write("Este es mi primer servidor");
-    response.end()
+    response.end
 })
+
+// const server = http.createServer((request,response)=>{
+
+//     const {url,method}= request
+//     if (method === "GET" && url === "/hola") {
+//         response.write("Estamos en el endpoint de hola")
+//     }
+//     if (method === "POST" && url === "/hola") {
+//         response.write("Estamos creando algo en hola")
+//     }
+//     if (method === "PUT" && url === "/hola") {
+//         response.write("Estamos sustituyendo algo en hola")
+//     }
+//     if (method === "PATCH" && url === "/hola") {
+//         response.write("Estamos parcheando algo en hola")
+//     }
+//     if (method === "DELETE" && url === "/hola") {
+//         response.write("Estamos borrando en hola")
+//     }
+    
+//     if (method === "GET" && url === "/adios") {
+//         response.write("Estamos en el endpoint de adios")
+//     }
+//     if (method === "POST" && url === "/adios") {
+//         response.write("Estamos posteando en adios")
+//     }
+//     if (method === "PUT" && url === "/adios") {
+//         response.write("Estamos sustituyendo en adios")
+//     }
+//     if (method === "PATCH" && url === "/adios") {
+//         response.write("Estamos parchando algo en adios")
+//     }
+//     if (method === "DELETE" && url === "/adios") {
+//         response.write("Estamos borrando en adios")
+//     }
+    
+//     // responder las request
+//     // response.write("Este es mi primer servidor");
+//     response.end()
+// })
 
 
 //prender el servidor 
-server.listen(8080,()=>{
+newServer.listen(8080,()=>{
     console.log("El servidor prendio")
 })
 
